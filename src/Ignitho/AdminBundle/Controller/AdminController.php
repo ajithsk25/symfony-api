54f36ef7d2bc75dc9a91005bd70e91d40b76f6e1
George J Padayatti <georgejpadayatty0000@gmail.com>
2017-07-05 14:25:05 +0530

    adding desc for admin homeAction()

--- src/Ignitho/AdminBundle/Controller/AdminController.php
+++ src/Ignitho/AdminBundle/Controller/AdminController.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AdminController extends Controller
 {
+    /**
+     * Displays the admin home.
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function homeAction()
     {
         return $this->render('IgnithoAdminBundle:Admin:home.html.twig', array(
